1f1be6d12fa1d469ef7b333d01daf6b287913f5f
D <166647954+mmjustme@users.noreply.github.com>
2024-09-19 13:45:32 +0000

    Updated some nuanses

--- 1_3/index.php
+++ 1_3/index.php
@@ -8,50 +8,57 @@
 <body>
     <h1>Constants & Variable variables in PHP</h1>
 
-    <h3>Constants</h3>
-    <p>оголошення константів через define("назва","значення")</p>
+    <h1>Constants</h1>
+    <h3>оголошення константів через define("назва","значення")</h3>
     
     <?php
-    define("STATUS_PAID", "paid");
-    echo "<br>", STATUS_PAID;
+        define("STATUS_PAID", "aproove");
+        echo "значення константи STATUS_PAID : ", STATUS_PAID; // aproove
 
-    # перевірка чи оголошено константу
-    # повертає 1(true) якщо так
-    echo "<br>",defined("STATUS_PAID");
-    # повертає нічого якщо ні
-    echo "<br>",defined("STATUS"); # пусто
-        
-    # оголошення через змінну (динамічно)
-    $add_status = "TWO";
-    define("STATUS_" . $add_status, $add_status);
-    echo "<br>",STATUS_TWO;  
+        echo "<h3>Перевірка чи оголошено константу через defined('назва константи')</h3>";
+        echo "повертає 1(true) якщо так: ", defined("STATUS_PAID");
+        echo "<br> повертає нічого якщо ні: ",defined("STATUS"); # пусто
+
+        echo "<h3>Oголошення констант через змінну (динамічно)</h3>";
+        $add_status = "TWO";
+        define("STATUS_" . $add_status, $add_status);
+        echo STATUS_TWO;  
     ?>
 
-    <p>оголошення константів через const, основна відмінність, що 
-    const не працює в виразах як if чи loop (детальніше в інших розділах)
-    </p>
+    <hr>
+    <h3>Оголошення константів через const, основна відмінність, що 
+    const не працює в виразах як "if" чи "loop" (детальніше в інших розділах)
+    </h3>
     <?php
-        const STATUS_ONE = "paid";
-        echo "<br>", STATUS_ONE;
+        const STATUS_ONE = "aproove";
+        echo "значення константи STATUS_ONE: ", STATUS_ONE;
     ?>
+<hr>
+<h3>Вбудовані константи</h3>
+    <?= "константа PHP_VERSION:", PHP_VERSION ?>
 
-
-<p>закладені константи</p>
-    <?php echo PHP_VERSION;?>
-
-    <p>магічні константи - константи в яких змінюється значення</p>
+<h3>Магічні константи</h3>
+<p>
+константи в яких змінюється значення в залежності від умови.
+    Наприклад від виклику константи в залежності на якому рядку
+</p>
     # повертає № рядку коду -  
     <?php 
-        echo __LINE__; // 44
-        echo '<br>', __FILE__; // розташування файлу
+        echo __LINE__; // 45
+        echo '<br>', __FILE__; // розташування файлу /project/workspace/1_3/index.php
     ?>
 
-
+<hr>
     <h3>Variable variables</h3>
-
+<p>Визначення назви змінної через значення іншої змінної. 
+    Тобто, створюємо змінну $foo зі значеням "bar"($foo = "bar"). </p>
     <?php
     
     $foo = "bar";
+
+    echo "<p>Стоврюємно наступну змінну зі значенням 'baz',
+     щоб присвоїти їй назву через значення першої змінної викор. $$. Отже, $$foo = 'baz'</p>";
+    
     $$foo = "baz";
     # даний запиз означає, що $$foo візьме як _назву_ змінної _значення_ $foo.
     # тобто це те саме що $bar="baz";
